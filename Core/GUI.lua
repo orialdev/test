8b76b2c57c6c43c183e9eedc4b6930f74188591c
orialdev <vitorgajogos@gmail.com>
2025-09-29 19:39:11 -0300

    test9 commit

--- Core/GUI.lua
+++ Core/GUI.lua
@@ -4,11 +4,26 @@ local GUI = {}
 -- Supondo que você está usando uma biblioteca de UI chamada 'OrionLib'
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local Window = WindUI:MakeWindow({
-    Name = "testing",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "MyHubConfig"
+local Window = WindUI:CreateWindow({
+    Title = "My Super Hub",
+    Icon = "door-open",
+    Author = "by .ftgs and .ftgs",
+    Folder = "MySuperHub",
+
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+    }
 })
 
 -- Criamos as abas aqui e as guardamos para que os módulos possam usá-las
