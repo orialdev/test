1ab6c2acd5fe0fdd88dce2f2bed8203d5b6a7c9f
orialdev <vitorgajogos@gmail.com>
2025-10-03 19:33:35 -0300

    section1

--- Features/Walkspeed.lua
+++ Features/Walkspeed.lua
@@ -44,6 +44,12 @@ end
 function WalkspeedModule:Init(targetTab)
     local section = targetTab:Section({ 
     Title = "Section",
+    Box = false,
+    FontWeight = "SemiBold",
+    TextTransparency = 0.05,
+    TextXAlignment = "Left",
+    TextSize = 17, -- Default Size
+    Opened = true,
 })
 
     section:Toggle({
